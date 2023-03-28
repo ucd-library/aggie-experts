4321479bbca47a06dd0c28988e99e309ba74b0e1
rakunkel-ucd <rakunkel@ucdavis.edu>
2023-03-28 15:57:56 -0700

    rm big files. Use IAM API call for profiles.

--- experts-api/import-iam-profiles.js
+++ experts-api/import-iam-profiles.js
@@ -8,72 +8,97 @@ import jsonld from 'jsonld';
 import fs from 'fs';
 import { Readable } from 'readable-stream';
 import { ClassicLevel } from 'classic-level';
+import * as https from 'https';
+import { exit } from 'process';
 
-// get a jsonld doc
-const doc = fs.readFileSync('faculty-sample.json', 'utf8');
-const docObj = JSON.parse(doc).responseData.results;
-const context = {
-    "@Version": 1.1,
-    "@base": "http://iam.ucdavis.edu/",
-    "@vocab": "http://iam.ucdavis.edu/schema#",
-    "iamId": "@id",
-    "orgOId": "@id",
-    "bouOrgOId": {
-        "@type": "@id"
-    },
-    "iam": "http://iam.ucdavis.edu/schema#",
-    "harvest_iam": "http://iam.ucdavis.edu/"
-};
-var ldJson = '{"@context":' + JSON.stringify(context) + ',"@id":"http://iam.ucdavis.edu/", "@graph":' + JSON.stringify(docObj) + '}';
+let request = https.get('https://iet-ws-stage.ucdavis.edu/api/iam/people/profile/search?key=***REMOVED***&isFaculty=true', (res) => {
+    if (res.statusCode !== 200) {
+        console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+        res.resume();
+        return;
+    }
+    res.on('data', (chunk) => {
+        doc += chunk;
+    });
 
-// Create a jsonld parser
-const myParser = new JsonLdParser();
+    var doc = '';
 
-// Any implementation of AbstractLevel can be used.
-const backend = new ClassicLevel('./db', { valueEncoding: 'json' });
+    res.on('close', () => {
+        console.log('Retrieved all profiles');
+        console.log(JSON.parse(doc));
+        processDoc(doc);
 
-// Implementation of the RDF/JS DataFactory interface
-const df = new DataFactory();
-// Store and query engine are separate modules
-const qstore = new Quadstore({ backend, dataFactory: df });
-const engine = new QueryEngine();
+    });
+});
 
-// Open the store
-await qstore.open();
+async function processDoc(doc) {
 
-// Import the jsonld into the parser
-async function import_via_put() {
+    //const doc = fs.readFileSync('faculty-sample.json', 'utf8');
+    const docObj = JSON.parse(doc).responseData.results;
+    const context = {
+        "@Version": 1.1,
+        "@base": "http://iam.ucdavis.edu/",
+        "@vocab": "http://iam.ucdavis.edu/schema#",
+        "iamId": "@id",
+        "orgOId": "@id",
+        "bouOrgOId": {
+            "@type": "@id"
+        },
+        "iam": "http://iam.ucdavis.edu/schema#",
+        "harvest_iam": "http://iam.ucdavis.edu/"
+    };
+    var ldJson = '{"@context":' + JSON.stringify(context) + ',"@id":"http://iam.ucdavis.edu/", "@graph":' + JSON.stringify(docObj) + '}';
 
-    myParser.import(Readable.from(ldJson))
-        .on('error', console.error)
-        .on('end', () => parsed())
-        .on('data', data => {
-            qstore.put(data);
-        });
+    // Create a jsonld parser
+    const myParser = new JsonLdParser();
 
-    async function parsed() {
-        console.log('All triples were parsed!');
-        const items = await qstore.get({});
-        //fs.writeFileSync('quadstore.json', JSON.stringify(items), 'utf8');
+    // Any implementation of AbstractLevel can be used.
+    const backend = new ClassicLevel('./db', { valueEncoding: 'json' });
 
-        qstore_query();
+    // Implementation of the RDF/JS DataFactory interface
+    const df = new DataFactory();
+    // Store and query engine are separate modules
+    const qstore = new Quadstore({ backend, dataFactory: df });
+    const engine = new QueryEngine();
+
+    // Open the store
+    await qstore.open();
+
+    // Import the jsonld into the parser
+    async function import_via_put() {
+
+        myParser.import(Readable.from(ldJson))
+            .on('error', console.error)
+            .on('end', () => parsed())
+            .on('data', data => {
+                qstore.put(data);
+            });
+
+        async function parsed() {
+            console.log('All triples were parsed!');
+            const items = await qstore.get({});
+            //fs.writeFileSync('quadstore.json', JSON.stringify(items), 'utf8');
+
+            qstore_query();
+        }
+
+    }
+
+    await import_via_put();
+
+    async function qstore_query() {
+
+        console.log('qstore_query');
+
+        const sparql = fs.readFileSync('./construct-vivo.sql', 'utf8');
+        //   const bindingsStream = await engine.queryBindings('SELECT * {graph ?g {?s ?p ?o}}');
+        const result = await engine.query(sparql, { sources: [qstore] });
+        const { data } = await engine.resultToString(result, 'application/ld+json');
+
+        var output = fs.createWriteStream("vivo.jsonld");
+
+        data.pipe(output);
+
     }
 
 }
-
- await import_via_put();
-
-async function qstore_query() {
-
-  console.log('qstore_query');
-
-  const sparql = fs.readFileSync('./construct-vivo.sql', 'utf8');
-//   const bindingsStream = await engine.queryBindings('SELECT * {graph ?g {?s ?p ?o}}');
-   const result = await engine.query(sparql, { sources:[qstore] });
-   const { data } = await engine.resultToString(result, 'application/ld+json');
-
-var output = fs.createWriteStream("vivo.jsonld");
-data.pipe(output);
-}
-
-
