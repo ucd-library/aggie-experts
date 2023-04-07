93fa1794060ee1afaf8485c895921f2f78c437dd
rakunkel-ucd <rakunkel@ucdavis.edu>
2023-04-07 15:43:36 -0700

    iam import dev

--- experts-client/bin/experts-iam.js
+++ experts-client/bin/experts-iam.js
@@ -3,24 +3,25 @@ import ExpertsClient from '../lib/experts-client.js';
 
 const program = new Command();
 
-program.command()
+program.command('getIam')
   .description('Import IAM Researcher Profiles')
-  .option('--iam-auth, -a <iam-auth>', 'API Key for IAM')
-  .option('--endpoint, -u <endpoint>', 'Endpoint for IAM', 'https://iet-ws-stage.ucdavis.edu/api/iam/people/profile/search?isFaculty=true')
-  .action((options) => {
+  .option('--iam-auth, -a <>', 'API Key for IAM')
+  .option('--endpoint, -e <>', 'Endpoint for IAM', 'https://iet-ws-stage.ucdavis.edu/api/iam/people/profile/search?isFaculty=true')
+  .action(async(options) => {
     const key = process.env.EXPERTS_IAM_AUTH;
     const endPoint = process.env.EXPERTS_IAM_ENDPOINT;
     
     const ec = new ExpertsClient(endPoint, key);
     
-    (async function () {
+    async function main() {
+        console.log('starting getIAMProfiles');
         ec.doc = await ec.getIAMProfiles();
-        console.log(ec.doc);
         console.log('done with getIAMProfiles');
     
         await ec.processDoc().then(() => {
             console.log('done with processDoc');
         })
-    })();
+    }
+    main();
 });
-program.parse();
+program.parse(process.argv);
