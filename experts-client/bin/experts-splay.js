60b0212463116e1ff75f34c2d3f789810d41a482
Quinn Hart <qjhart@ucdavis.edu>
2023-04-11 12:44:07 -0700

    Updated splay for quadstore

--- experts-client/bin/experts-splay.js
+++ experts-client/bin/experts-splay.js
@@ -1,7 +1,10 @@
 'use strict';
 import fs from 'fs-extra';
 import { Command} from 'commander';
+import { Engine } from 'quadstore-comunica';
 import { QueryEngine } from '@comunica/query-sparql';
+import { localDB } from '../lib/experts-client.js';
+import { DataFactory } from 'rdf-data-factory';
 
 import JsonLdProcessor from 'jsonld';
 const jsonld=new JsonLdProcessor();
@@ -20,6 +23,7 @@ function str_or_file(opt,param,required) {
     return null;
   }
 }
+
 const program = new Command();
 
 program.name('splay')
@@ -32,6 +36,7 @@ program.name('splay')
   .option('--frame <frame>', 'frame object for each binding')
   .option('--frame@ <frame.json>', 'file containing frame on the construct')
   .option('--source <source...>', 'Specify linked data source. Can be specified multiple times')
+  .option('--quadstore <quadstore>', 'Specify a local quadstore.  Cannot be used with the --source option')
 
 program.parse(process.argv);
 
@@ -49,36 +54,51 @@ if (cli.frame) {
   cli.frame=JSON.parse(cli.frame);
 }
 
-const q = new QueryEngine();
+let q;
+let sources=null;
+if (cli.quadstore) {
+  const db = await localDB.create({level:'ClassicLevel',path:cli.quadstore});
+//  cli.source=[db];
+  q = new Engine(db.store);
+  sources=null;
+} else {
+  q = new QueryEngine();
+  sources=cli.source;
+}
+
+const factory=new DataFactory();
 
 const bindingStream=await q.queryBindings(cli.bind,{sources: cli.source})
+//const bindingStream=await q.queryBindings(cli.bind)
 bindingStream
   .on('data', async (bindings) => {
+    let fn=1; // write to stdout by default
+    console.log(bindings.toString());
     if ( bindings.get('filename') && bindings.get('filename').value) {
-      const fn=bindings.get('filename').value
-      const quadStream = await q.queryQuads(cli.construct,{initialBindings:bindings, sources: cli.source});
-      if (frame) {
-        const quads = await quadStream.toArray();
-        console.log(`writing ${fn} with ${quads.length} quads`);
-        const doc=await jsonld.fromRDF(quads)
-        const framed=await jsonld.frame(doc,cli.frame,{omitGraph:false,safe:true})
-        if (bindings.get('uri')) {
-          framed['@id']=bindings.get('uri').value;
-        }
-        fs.writeFileSync(fn,JSON.stringify(framed,null,2));
-      } else {
-        const c = await q.query(construct,{initialBindings:bindings,sources: cli.source});
-        const {data} = await q.resultToString(c,'application/ld+json');
-        await fs.ensureFile(fn)
-        const writeStream = fs.createWriteStream(fn);
-        data.pipe(writeStream);
-        data.on('end', () => {
-          console.log('done writing '+fn);
-        });
-      }
-    } else {
-      console.log('no filename');
+      fn=bindings.get('filename').value
     }
+    let graph = null;
+    if (bindings.get('graph')) {
+      graph=factory.namedNode(bindings.get('graph').value);
+    }
+
+    // convert construct to jsonld quads
+    const quadStream = await q.queryQuads(cli.construct,{initialBindings:bindings, sources: cli.source});
+    const quads = await quadStream.toArray();
+    if (graph) {
+      quads.forEach((quad) => {
+        quad.graph=graph;
+      });
+    }
+    console.log(`writing ${fn} with ${quads.length} quads`);
+    let doc=await jsonld.fromRDF(quads)
+
+    if (frame) {
+      doc=await jsonld.frame(doc,cli.frame,{omitGraph:false,safe:true})
+    } else {
+      //      doc=await jsonld.expand(doc,{omitGraph:false,safe:true})
+    }
+    fs.writeFileSync(fn,JSON.stringify(doc,null,2));
   })
   .on('error', (error) => {
     console.error(error);
