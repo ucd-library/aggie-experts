6932929dff266fb7f384fb5c51d75465833ee5df
Quinn Hart <qjhart@ucdavis.edu>
2023-05-16 18:19:01 -0700

    Qh homepage (#120)
    * Added attempt at homepage
    
    * Updated base-service client
    
    * Added context file

---
+++ services/base-service/spa/client/public/elements/pages/about/app-about.js
@@ -0,0 +1,34 @@
+import { LitElement, html } from 'lit';
+import render from "./app-about.tpl.js";
+
+class AppAbout extends Mixin(LitElement)
+      .with(LitCorkUtils) {
+
+  static get properties() {
+    return {
+
+    };
+  }
+
+  constructor() {
+    super();
+    this.render = render.bind(this);
+    this.active = true;
+    this._injectModel('AppStateModel', 'CollectionModel', 'RecordModel');
+  }
+
+  /**
+   * @method _onSearch
+   * @description called from the search box button is clicked or
+   * the enter key is hit.  set the text filter
+   * @param {Object} e
+   */
+   _onSearch(e) {
+    let searchDoc = this.RecordModel.emptySearchDocument();
+    this.RecordModel.setTextFilter(searchDoc, e.detail);
+    this.RecordModel.setSearchLocation(searchDoc);
+  }
+
+}
+
+customElements.define('app-about', AppAbout);
