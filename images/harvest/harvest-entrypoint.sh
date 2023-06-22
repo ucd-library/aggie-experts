3865f0beaedaf16131cdd4e5788a541acf6a20c8
Quinn Hart <qjhart@ucdavis.edu>
2023-06-22 14:56:45 -0700

    Updated the harvest image

--- images/harvest/harvest-entrypoint.sh
+++ images/harvest/harvest-entrypoint.sh
@@ -5,10 +5,19 @@
 
 init_local_user
 
+function gcloud_auth() {
+  if [[ -n ${GOOGLE_APPLICATION_CREDENTIALS_JSON} ]]; then
+    gcloud auth activate-service-account --key-file=- <<< "${GOOGLE_APPLICATION_CREDENTIALS_JSON}"
+  else
+    echo "GOOGLE_APPLICATION_CREDENTIALS_JSON is not set"
+  fi
+}
+
 uid=$(id -u)
 if [[ ${uid} = 0 ]]; then
   # Don't cd, because users may want to set their own workdir
-  exec setpriv --reuid=ucd.process --init-groups make --file=/usr/local/lib/harvest/harvest.mk "$@"
+  setpriv --reuid=ucd.process --init-groups -- bash -c 'gcloud auth activate-service-account --project=digital-ucdavis-edu --key-file=- <<< "${GOOGLE_APPLICATION_CREDENTIALS_JSON}"'
+  exec setpriv --reuid=ucd.process --init-groups make --file=/usr/local/lib/harvest/Makefile "$@"
 else
-  exec make --file=/usr/local/lib/harvest/harvest.mk "$@"
+  exec make --file=/usr/local/lib/harvest/Makefile "$@"
 fi
