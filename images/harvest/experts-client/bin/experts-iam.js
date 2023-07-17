28cdaea5ae49b236399fe35ab78a3b4e3a195885
Quinn Hart <qjhart@ucdavis.edu>
2023-07-17 11:12:28 -0700

    Template for updates

--- images/harvest/experts-client/bin/experts-iam.js
+++ images/harvest/experts-client/bin/experts-iam.js
@@ -46,8 +46,6 @@ async function main(opt) {
 
   const ec = new ExpertsClient(opt);
 
-  console.log('starting getIAMProfiles');
-
   await ec.getIAMProfiles(opt);
 
   console.log('starting createJsonLd');
@@ -63,9 +61,11 @@ async function main(opt) {
   await ec.createDataset(opt);
   console.log(`Dataset '${opt.fuseki.db}' created successfully.`);
 
-  console.log('starting createGraph');
-  await ec.createGraphFromJsonLdFile(ec.jsonld, opt);
-  console.log(`Graph created successfully in dataset '${opt.fuseki.db}'.`);
+//  This part should use are (now standard) insert_iam query.
+
+//  console.log('starting createGraph');
+//  await ec.createGraphFromJsonLdFile(ec.jsonld, opt);
+//  console.log(`Graph created successfully in dataset '${opt.fuseki.db}'.`);
 
   // Any other value don't delete
   if (opt.fuseki.isTmp === true && !opt.saveTmp) {
@@ -80,22 +80,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename).replace('/bin', '/lib');
 
 
-program.name('iam')
-  .usage('[options] <file...>')
+program.name('experts-iam')
+  .usage('[options] <experts...>')
+// If we have any casId's, on the cmdline, then we assumed --no-staff and --no-faculty and only process those casId's
   .description('Import IAM Researcher Profiles')
-  .option('--iam-auth <key>', 'UC Davis IAM authentication key')
-  .option('--userId <userId>', 'UC Davis IAM user id')
-  .option('--iam-endpoint <endpoint>', 'UC Davis IAM endpoint', 'https://iet-ws-stage.ucdavis.edu/api/iam/people/profile/search')
-  .option('--bind <bind>', 'select query for binding')
-  .option('--bind@ <bind.rq>', 'file containing select query for binding', __dirname + '/query/person/bind.rq')
-  .option('--construct <construct>', 'construct query for each binding')
-  .option('--construct@ <construct.rq>', 'file containing construct query for each binding', __dirname + '/query/person/construct.rq')
-  .option('--frame <frame>', 'frame object for each binding')
-  .option('--frame@ <frame.json>', 'file containing frame on the construct')
+  .option('--environment <env>', 'specify environment', 'production')
+// IAM endpoint and auth are now in Google Secret Manager, and a function of the environment.
+  .option('--staff', 'Include staff',true)
+// Whether to download all staff
+  .option('--faculty', 'Include faculty',true)
+// Whether to download all faculty
   .option('--source <source...>', 'Specify linked data source. Can be specified multiple times')
-  .option('--quadstore <quadstore>', 'Specify a local quadstore.  Cannot be used with the --source option')
   .option('--fuseki.isTmp', 'create a temporary store, and files to it, and unshift to sources before splay.  Any option means do not remove on completion', false)
   .option('--fuseki.type <type>', 'specify type on --fuseki.isTmp creation', 'tdb')
+// Fuseki type, defaults to tdb for sure for IAM.
   .option('--fuseki.url <url>', 'fuseki url', fuseki.url)
   .option('--fuseki.auth <auth>', 'fuseki authorization', fuseki.auth)
   .option('--fuseki.db <name>', 'specify db on --fuseki.isTmp creation.  If not specified, a random db is generated', fuseki.db)
