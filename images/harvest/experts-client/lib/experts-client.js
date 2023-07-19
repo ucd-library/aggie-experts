afe33baea302b75160f79f3700d178f074913093
Quinn Hart <qjhart@ucdavis.edu>
2023-07-19 16:43:26 -0700

    save

--- images/harvest/experts-client/lib/experts-client.js
+++ images/harvest/experts-client/lib/experts-client.js
@@ -371,8 +371,7 @@ export class ExpertsClient {
       const quads = await quadStream.toArray();
       let doc = await jp.fromRDF(quads)
       if (frame) {
-        //        doc = await jp.frame(doc, opt.frame, { omitGraph: false, safe: true })
-        doc = await jp.frame(doc, opt.frame, { omitGraph: true, safe: true })
+        doc = await jp.frame(doc, opt.frame, { omitGraph: false, safe: true })
       } else {
         doc = await jp.expand(doc, { omitGraph: false, safe: true })
       }
