c92e0a5678610bfa99a22dc1b5528bd02b5dcdc6
rakunkel-ucd <rakunkel@ucdavis.edu>
2023-09-29 17:08:58 -0700

    since on relation calls

--- harvest/experts-client/bin/experts-cdl.js
+++ harvest/experts-client/bin/experts-cdl.js
@@ -71,9 +71,55 @@ async function main(opt) {
 
   const users = program.args;
 
+  // Step 1: Get Usernames from CDL using any cmd line args
+  var uquery = 'users?detail=ref&per-page=1000';
+  var sinceFilter = '';
+
+  if (opt.username) {
+    uquery += '&username=' + opt.username;
+  }
+  else if (opt.cdl.groups) {
+    uquery += '&groups=' + opt.cdl.groups;
+  }
+
+  if (opt.cdl.affected !== undefined && opt.cdl.affected !== null) {
+    // We need the date in XML ISO format
+    var date = new Date();
+    date.setDate(date.getDate() - opt.cdl.affected); // Subtracts days
+    sinceFilter = '&affected-since=' + date.toISOString();
+    uquery += sinceFilter;
+  }
+  else if (opt.cdl.modified !== undefined && opt.cdl.modified !== null) {
+    // We need the date in XML ISO format
+    var date = new Date();
+    date.setDate(date.getDate() - opt.cdl.modified); // Subtracts days
+    sinceFilter = '&modified-since=' + date.toISOString();
+    uquery += sinceFilter;
+  }
+  console.log('uquery', uquery);
+  // return;
+
+  // Get the users from CDL that meet the criteria
+  const entries = await ec.getCDLentries(opt, uquery);
+
+  // Add them to the users array
+  for (let entry of entries) {
+    entry = entry['api:object'];
+    users.push(entry['username'].substring(0, entry['username'].indexOf('@')));
+  }
+
+
+  console.log(users.join(' '));
+  console.log(users.length + ' users found');
+  // const usrs = users.slice(0, 5);
+  // console.log(usrs.join(' '));
+  // return;
+
+  // Step 2: Get User Profiles and relationships from CDL
   for (const user of users) {
 
     if (opt.fuseki.isTmp) {
+      // Create a random db name if not specified
       //console.log('starting createDataset');
       opt.fuseki.db = user + '-' + nanoid(5);
       const fuseki = await ec.mkFusekiTmpDb(opt);
@@ -83,13 +129,16 @@ async function main(opt) {
 
     console.log('starting getCDLprofile ' + user);
 
+    // Get a full profile for the user
     const entries = await ec.getCDLentries(opt, 'users?username=' + user + '@ucdavis.edu&detail=full');
     // Assume a single entry for a user profile, but it's an array
     ec.profile = entries;
 
     let cdlId = entries[0]["api:object"].id;
 
-    console.log('starting createJsonLd ' + user);
+    console.log('starting createJsonLd ' + user + '-' + cdlId);
+
+    // Create the JSON-LD for the user profile
     let contextObj = context;
 
     contextObj["@id"] = 'http://oapolicy.universityofcalifornia.edu/';
@@ -98,15 +147,17 @@ async function main(opt) {
     let jsonld = JSON.stringify(contextObj);
     console.log('starting createGraph ' + user);
 
+    // Insert into our local Fuseki
     await ec.createGraphFromJsonLdFile(jsonld, opt);
 
     console.log(`Graph created successfully in dataset '${opt.fuseki.db}'.`);
-
     console.log('starting getCDLentries ' + user + '-' + cdlId);
 
-    // fetch publications for user
+    // fetch all relations for user. Note that the may be grants, etc.
     ec.works = [];
-    let works = await ec.getCDLentries(opt, 'users/' + cdlId + '/relationships?detail=full');
+
+    let works = await ec.getCDLentries(opt, 'users/' + cdlId + '/relationships?detail=full' + sinceFilter);
+
 
     for (let work of works) {
       let related = [];
@@ -119,6 +170,8 @@ async function main(opt) {
       ec.works.push(work['api:relationship']);
     }
 
+    console.log(ec.works.length + ' works found for ' + user);
+
     console.log('starting createJsonLd ' + user);
     contextObj = context;
     contextObj["@id"] = 'http://oapolicy.universityofcalifornia.edu/';
@@ -128,6 +181,7 @@ async function main(opt) {
     console.log('starting works createGraph ' + user);
 
     await ec.createGraphFromJsonLdFile(jsonld, opt);
+    fs.writeFileSync(path.join(__dirname, '../data', user + '.jsonld'), jsonld);
 
     console.log(`Graph created successfully in dataset '${opt.fuseki.db}'.`);
 
@@ -169,6 +223,9 @@ program.name('cdl-profile')
   .option('--source <source...>', 'Specify linked data source. Used instead of --fuseki')
   .option('--output <output>', 'output directory')
   .option('--cdl.url <url>', 'Specify CDL endpoint', cdl.url)
+  .option('--cdl.groups <groups>', 'Specify CDL group ids', cdl.groups)
+  .option('--cdl.affected <affected>', 'affected since')
+  .option('--cdl.modified <modified>', 'modified since')
   .option('--cdl.auth <user:password>', 'Specify CDL authorization', cdl.auth)
   .option('--experts-service <experts-service>', 'Experts Sparql Endpoint', 'http://localhost:3030/experts/sparql')
   .option('--fuseki.isTmp', 'create a temporary store, and files to it, and unshift to sources before splay.  Any option means do not remove on completion', true)
