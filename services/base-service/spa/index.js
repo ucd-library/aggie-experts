b50d55a0db45bdbf150134058b4d1b410471479e
UcDust <dcartwright@ucdavis.edu>
2023-11-03 09:55:46 -0700

    experiment with app.use for login.html

--- services/base-service/spa/index.js
+++ services/base-service/spa/index.js
@@ -1,5 +1,39 @@
 const express = require('express');
+// const {keycloak} = require('@ucd-lib/fin-service-utils');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const compression = require('compression');
+const path = require('path');
+
 const app = express();
+
+// parse cookies and add compression
+// app.use(cookieParser());
+// app.use(compression());
+
+// parse application/x-www-form-urlencoded req body
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json req body
+// app.use(bodyParser.json());
+
+// app.use(keycloak.setUser);
+
+// path to your spa assets dir
+let assetsDir = path.join(__dirname, 'client', 'public');
+
+app.use((req, res, next) => {
+  let user = req.get('x-fin-user');
+  if( typeof user === 'string' ) user = JSON.parse(user);
+
+  if( !user || !user['preferred_username'] ) {
+    // res.redirect('/');
+    res.sendFile(assetsDir + '/login.html');
+    return;
+  }
+
+  next();
+});
 
 // setup static routes
 require('./controllers/static')(app);
