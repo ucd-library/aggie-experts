5a3df8caf4a5dc7cef46e234945f5de5700a60ba
Quinn Hart <qjhart@ucdavis.edu>
2024-01-17 10:03:57 -0800

    Making model changes

--- services/base-service/models/authorship/model.js
+++ services/base-service/models/authorship/model.js
@@ -32,6 +32,69 @@ class AuthorshipModel extends BaseModel {
   }
 
   /**
+   * @method favourite
+   * @description Favourite the given id.
+   * @param {Object} args :  { id: id to favourite }
+   * @returns {Object} : document object
+    **/
+  async favourite(args) {
+    const { id, objectId, favourite } = args;
+    const doc=this.client_get(id);
+
+    const root_node= this.get_expected_model_node(transformed);
+    const doc = this.promote_node_to_doc(root_node);
+    logger.info(`AuthorshipModel.favorite(${doc['@id']})`);
+    await this.update_or_create_main_node_doc(doc);
+
+    const have_part={};
+    // Get the work and the Expert via the Authorship.relates
+    const expertModel= await this.get_model('expert');
+    const workModel= await this.get_model('work');
+    // The root_node is the Authorship node, pointers to Expert and Work (which is now another node)
+    for(let i=0; i<root_node?.relates?.length || 0; i++) {
+      let relates = root_node.relates[i];
+      try {
+        // Is this a Expert?
+        let related = await expertModel.client_get(relates);
+        let type = this.experts_node_type(related);
+        if (type !== 'Expert') {
+          throw new Error(`AuthorshipModel.update(${doc['@id']}) - ${relates} is not a Expert`);
+        }
+        have_part['Expert'] = {id:relates,node:related };
+      } catch (e) {
+        // Is this included Work?
+        let related = workModel.get_expected_model_node(transformed);
+        let type = this.experts_node_type(related);
+        if (relates !== related['@id']) {
+          throw new Error(`AuthorshipModel.update(${relates} not included in doc`);
+        }
+        if (type !== 'Work') {
+          throw new Error(`AuthorshipModel.update(${relates} is not a Work`);
+        }
+        have_part['Work'] = {id:relates,node:related };
+      }
+      if (have_part.Expert && have_part.Work) {
+        // Add Work as snippet to Expert
+        logger.info(`${have_part.Expert.id} ==> ${have_part.Work.id}`);
+        {
+          const node = workModel.snippet(have_part.Work.node);
+          await expertModel.update_graph_node(have_part.Expert.id,node);
+        }
+      } else {
+        if (have_part.Expert) {
+          logger.info(`${have_part.Expert.id} =>? ?Work?`);
+        } else {
+          if (have_part.Work) {
+            logger.info(`?Expert? ?=> ${have_part.Work.id}`);
+          } else {
+            logger.info(`?Expert? ?=? ?Work?`);
+          }
+        }
+      }
+    }
+  }
+
+  /**
    * @method update
    * @description Update Elasticsearch with the given data.
    */
