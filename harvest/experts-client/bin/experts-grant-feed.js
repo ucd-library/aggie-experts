1af9f697884cf9379b64ed6ca6b313d96ec58040
rakunkel-ucd <rakunkel@ucdavis.edu>
2024-01-17 16:48:58 -0800

    Downloads file from aggie-enterprise bucket and transfers to Symplectic

--- harvest/experts-client/bin/experts-grant-feed.js
+++ harvest/experts-client/bin/experts-grant-feed.js
@@ -2,8 +2,10 @@
 import fs from 'fs';
 import path from 'path';
 import { Command } from 'commander';
+import FusekiClient from '../lib/fuseki-client.js';
 import Client from 'ssh2-sftp-client';
-
+import { Storage } from '@google-cloud/storage';
+// import fs from 'fs/promises';
 import { GoogleSecret } from '@ucd-lib/experts-api';
 
 const gs = new GoogleSecret();
@@ -13,10 +15,12 @@ const program = new Command();
 program
   .version('1.0.0')
   .description('Upload a file to a remote SFTP server')
+  .requiredOption('-b, --bucket <bucket>', 'GCS bucket name')
+  .requiredOption('-s, --source <source>', 'Source file path in GCS')
+  .requiredOption('-o, --output <output>', 'Local output file path')
   .requiredOption('-h, --host <host>', 'SFTP server hostname')
   .requiredOption('-u, --username <username>', 'SFTP username')
-  .requiredOption('-l, --local <local>', 'test.csv')
-  .requiredOption('-r, --remote <remote>', 'Remote file path on the server')
+  .requiredOption('-r, --remote <remote>', 'Remote file path on the Symplectic server')
   .parse(process.argv);
 
 let opt = program.opts();
@@ -27,7 +31,21 @@ const sftpConfig = {
   username: opt.username,
 };
 
-const localFilePath = path.resolve(opt.local);
+const storage = new Storage({
+  projectId: 'aggie-experts',
+  // keyFilename: 'path/to/your/keyfile.json',
+});
+
+const fuseki = new FusekiClient({
+  url: process.env.EXPERTS_FUSEKI_URL || 'http://localhost:3030',
+  auth: process.env.EXPERTS_FUSEKI_AUTH || 'admin:testing123',
+  type: 'tdb2',
+  db: 'CAS',
+  replace: true,
+  'delete': false
+});
+
+var localFilePath = ''; // A default local file path
 const remoteFilePath = opt.remote;
 
 opt.secretpath = 'projects/325574696734/secrets/Symplectic-Elements-FTP-ucdavis-password';
@@ -48,9 +66,40 @@ async function uploadFile() {
   }
 }
 
+async function downloadFile(bucketName, fileName, destinationPath) {
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(fileName);
+
+  try {
+    // Create a read stream from the file and pipe it to a local file
+    const readStream = file.createReadStream();
+    const writeStream = fs.createWriteStream(destinationPath);
+
+    readStream.on('error', err => {
+      console.error('Error reading stream:', err);
+    });
+
+    writeStream.on('finish', () => {
+      console.log(`File downloaded successfully to: ${destinationPath}`);
+    });
+
+    await readStream.pipe(writeStream);
+  } catch (error) {
+    console.error('Error downloading file:', error.message);
+  }
+}
+
 async function main(opt) {
 
+  // Retrieve the grants source file from GCS
+  localFilePath = opt.output + opt.source; // The file to be transferred to the Symplectic server
+  // First download the file from GCS
+  await downloadFile(opt.bucket, opt.source, localFilePath);
+
+  // Retrieve the SFTP password from GCS Secret Manager
   sftpConfig.password = await gs.getSecret(opt.secretpath);
+
+  // Perform the SFTP upload
   uploadFile();
 
 }
