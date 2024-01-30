a19940a622d31d0a6a5dbed0e86ba9f51ffe6c58
Quinn Hart <qjhart@ucdavis.edu>
2024-01-30 15:58:37 -0800

    Added user_can_edit

--- services/base-service/models/expert/api.js
+++ services/base-service/models/expert/api.js
@@ -4,13 +4,24 @@ const ExpertModel = require('./model.js');
 const {defaultEsApiGenerator} = dataModels;
 const md5 = require('md5');
 
+function user_can_edit(req, res, next) {
+  let id = '/'+model.id+decodeURIComponent(req.path);
+  // logger.info('Checking if user can edit', id, req.user);
+  if (req.user &&
+      (id === '/expert/'+md5(req.user.preferred_username+"@ucdavis.edu") ||
+       req.user?.roles?.includes('admin'))
+     ) {
+    return next();
+  }
+  res.status(403).send('Forbidden');
+}
+
 // Custom middleware to check Content-Type
 function json_only(req, res, next) {
   const contentType = req.get('Content-Type');
-
   if (contentType === 'application/json' || contentType === 'application/ld+json') {
     // Content-Type is acceptable
-    next();
+    return next();
   } else {
     // Content-Type is not acceptable
     res.status(400).json({ error: 'Invalid Content-Type. Only application/json or application/ld+json is allowed.' });
@@ -24,7 +35,7 @@ async function sanitize(req, res) {
       (id === '/expert/'+md5(req.user.preferred_username+"@ucdavis.edu") ||
        req.user?.roles?.includes('admin'))
      ) {
-    res.status(200).json(res.thisDoc);
+    return next();
   } else {
     let doc = res.thisDoc;
     for(let i=0; i<doc["@graph"].length; i++) {
@@ -49,16 +60,18 @@ async function sanitize(req, res) {
         delete doc["@graph"][i]["totalAwardAmount"];
       }
     }
-    res.status(200).json(doc);
+    res.thisDoc = doc;
+    return next();
   }
 }
 
 router.route(
   /expert\/[a-zA-Z0-9]+\/ark\:\/87287\/d7mh2m\/relationship\/[0-9]+/
 ).get(
+  user_can_edit,
   async (req, res, next) => {
     //    res.status(200).json(JSON.stringify(req));
-
+    logger.info({function:"GET /expert/:id/ark:/87287/d7mh2m/relationship/:id"},`req.path=${req.path}`);
     let pathParts = decodeURIComponent(req.path).split('/');
     let id = '/' + model.id + '/' + pathParts.splice(3).join('/');
 
@@ -68,14 +81,17 @@ router.route(
         admin : req.query.admin ? true : false,
       }
       res.thisDoc = await authorship_model.get(id, opts);
-
-      next();
+      logger.info({function:'get'},JSON.stringify(res.thisDoc));
+      return next();
     } catch(e) {
-      res.status(404).json(`${id} from ${req.path} HELP ${e.message}`);
+     res.status(404).json(`${id} from ${req.path} HELP ${e.message}`);
     }
   },
-  sanitize
-).patch(   // TODO: Dusty add person checking,
+  async (req, res, next) => {
+   res.status(200).json(res.thisDoc);
+  }
+).patch(
+  user_can_edit,
   json_only,
   async (req, res, next) => {
     logger.info(`PATCH ${req.url}`);
@@ -88,7 +104,6 @@ router.route(
 
     const authorshipModel = await model.get_model('authorship');
     await authorshipModel.patch(data,expertId);
-
     res.status(200).json({status: "ok"});
   }
 )
@@ -109,6 +124,7 @@ router.get(
       res.status(404).json(`${req.path} resource not found`);
     }
   },
+  sanitize,
   (req, res, next) => {
    res.status(200).json(res.thisDoc);
   }
