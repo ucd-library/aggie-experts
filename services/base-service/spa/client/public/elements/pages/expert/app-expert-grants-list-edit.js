604b18cb8e36d92a0a7b026a554bf12ed1e33162
UcDust <dcartwright@ucdavis.edu>
2024-03-04 13:25:07 -0800

    fix missing expert title role, fix edit grant download for relationships

--- services/base-service/spa/client/public/elements/pages/expert/app-expert-grants-list-edit.js
+++ services/base-service/spa/client/public/elements/pages/expert/app-expert-grants-list-edit.js
@@ -261,13 +261,13 @@ export default class AppExpertGrantsListEdit extends Mixin(LitElement)
         '"' + (grant.dateTimeInterval?.end?.dateTime || '') + '"',    // End date
         '"' + (grant.type || '') + '"',                               // Type of Grant
         '"' + (grant.role || '') + '"',                               // Role of Grant
-        '?', // List of contributors (role) {separate contributors by ";"}
+        '"' + grant.contributors.map(c => c.name).join('; ') + '"',   // List of contributors (PIs and CoPIs)
       ]);
     });
 
     if( !body.length ) return;
 
-    let headers = ['Title', 'Funding Agency', 'Grant Id', 'Start Date', 'End Date', 'Type of Grant', 'Role', 'List of Contributors'];
+    let headers = ['Title', 'Funding Agency', 'Grant Id', 'Start Date', 'End Date', 'Type of Grant', 'Role', 'List of PIs and CoPIs'];
     let text = headers.join(',') + '\n';
     body.forEach(row => {
       text += row.join(',') + '\n';
