947ce84dd3faa70eea062affdc0b02114d48b34a
Roger Alan Kunkel <125080780+rakunkel-ucd@users.noreply.github.com>
2024-03-08 14:33:19 -0800

    SiteFarm Experts Query with multiple IDs (#359)

---
+++ services/base-service/models/sitefarm/index.js
@@ -0,0 +1,8 @@
+const SiteFarmModel = require('./model.js');
+module.exports = {
+  api: require('./api.js'),
+  model: new SiteFarmModel(),
+  schema: require('../base/schema/minimal.json'),
+  transform: require('../base/transform.js'),
+  swagger: 'sitefarm.yaml'
+}
