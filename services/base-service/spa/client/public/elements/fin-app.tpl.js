987321a098e28748c210f0b993f40dbcdf8eebb8
UcDust <dcartwright@ucdavis.edu>
2024-03-15 13:29:07 -0700

    update theme package for quicklinks profile icon

--- services/base-service/spa/client/public/elements/fin-app.tpl.js
+++ services/base-service/spa/client/public/elements/fin-app.tpl.js
@@ -157,8 +157,10 @@ return html`
     <ucd-theme-quick-links
         title="My Account"
         style-modifiers="highlight"
+        use-icon
         @item-click="${e => console.log('@item-click', e.detail)}"
         @toggle="${e => console.log('@toggle', e.detail)}">
+      <svg slot="custom-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
       <a href="/${this.expertId}">Profile</a>
       <a href="/faq">Help</a>
       <a href="/auth/logout">Log Out</a>
