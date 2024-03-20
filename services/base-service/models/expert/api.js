a71f93aaca2ca44a15dbf8823baab154397d1c0c
Quinn Hart <qjhart@ucdavis.edu>
2024-03-20 12:05:03 -0700

    user_can_edit

--- services/base-service/models/expert/api.js
+++ services/base-service/models/expert/api.js
@@ -7,11 +7,15 @@ const md5 = require('md5');
 
 
 function user_can_edit(req, res, next) {
-  let id = '/'+model.id+decodeURIComponent(req.path);
+  let id = decodeURIComponent(req.path);
+  console.log('usercanedit', id);//
   // logger.info('Checking if user can edit', id, req.user);
+  id = id.split('/').slice(0,3).join('/');
+  console.log('usercanedit', id);//
+  console.log('usercanedit', '/expert/'+md5(req.user.preferred_username+"@ucdavis.edu"));
   if (req.user &&
-      (id === '/expert/'+md5(req.user.preferred_username+"@ucdavis.edu") ||
-       req.user?.roles?.includes('admin'))
+      (id === '/expert/'+md5(req.user.preferred_username+"@ucdavis.edu")) ||
+       req.user?.roles?.includes('admin')
      ) {
     return next();
   }
