9b498b4a1b987bc51f6ff32aa61971aff94ab742
Quinn Hart <qjhart@ucdavis.edu>
2024-04-09 23:32:18 -0700

    revert bad overwrites

--- harvest/experts-client/lib/experts-commander.js
+++ harvest/experts-client/lib/experts-commander.js
@@ -1,5 +1,7 @@
 import { Command as OriginalCommand, Option } from 'commander';
 import { ElementsClient } from '@ucd-lib/experts-api';
+import { logger } from './logger.js';
+import { IAM } from './iam-client.js';
 
 export class Command extends OriginalCommand {
   constructor(...args) {
@@ -11,10 +13,29 @@ export class Command extends OriginalCommand {
     return this;
   }
 
+  option_iam() {
+    this.addOption(new Option('--iam <dev|prod>', 'iam environment').choices(['dev', 'prod']).default('prod'));
+    this.addOption(new Option('--iam.timeout <timeout>', 'Specify IAM API timeout in milliseconds').default(30000))
+    return this;
+  }
+
+  option_fuseki() {
+    this.addOption(new Option('--fuseki <http://admin:testing123@localhost:3030>', 'fuseki authentication and location').default('http://admin:testing123@localhost:3030'));
+    return this;
+  }
+
+  option_log() {
+    this.addOption(new Option('--log <>', 'log level').choices(['info', 'warn','error','fatal']).default('info'));
+    return this;
+  }
+
   opts() {
     const opts=super.opts();
     if (opts.cdl) {
       opts.cdl=ElementsClient.info(opts.cdl);
+    }
+    if (opts.iam) {
+      opts.iam=IAM.info(opts.iam);
     }
     return opts;
   }
