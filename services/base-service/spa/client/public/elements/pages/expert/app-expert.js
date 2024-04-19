ac278ca4ba922f3ffd0b69dd3de924df1eb5a6ce
UcDust <dcartwright@ucdavis.edu>
2024-04-18 17:26:26 -0700

    more link icon logic into main loop

--- services/base-service/spa/client/public/elements/pages/expert/app-expert.js
+++ services/base-service/spa/client/public/elements/pages/expert/app-expert.js
@@ -160,27 +160,27 @@ export default class AppExpert extends Mixin(LitElement)
         if( !url.name ) {
           // remove http(s)://, www., and trailing slashes
           url.name = url.url?.replace(/^(http:\/\/|https:\/\/)|www\.|\/*$/g, '');
+        }
 
-          try {
-            // also set custom icon depending on type of website
-            if( url['@type'].includes('URL_googlescholar') ) {
-              url.icon = 'fa-google-scholar';
-            } else if( url['@type'].includes('URL_researchgate') ) {
-              url.icon = 'fa-researchgate';
-            } else if( url['@type'].includes('URL_linkedin') ) {
-              url.icon = 'fa-linkedin';
-            } else if( url['@type'].includes('URL_twitter') ) {
-              url.icon = 'fa-x-twitter';
-            } else if( url['@type'].includes('URL_mendeley') ) {
-              url.icon = 'fa-mendeley';
-            } else if( url['@type'].includes('URL_rssfeed') ) {
-              url.icon = 'fa-square-rss';
-            } else if( url['@type'].includes('URL_figshare') ) {
-              url.icon = 'ai-figshare';
-            }
-          } catch(e) {
-            console.warn('error setting website icon', e);
+        try {
+          // also set custom icon depending on type of website
+          if( url['@type'].includes('URL_googlescholar') ) {
+            url.icon = 'fa-google-scholar';
+          } else if( url['@type'].includes('URL_researchgate') ) {
+            url.icon = 'fa-researchgate';
+          } else if( url['@type'].includes('URL_linkedin') ) {
+            url.icon = 'fa-linkedin';
+          } else if( url['@type'].includes('URL_twitter') ) {
+            url.icon = 'fa-x-twitter';
+          } else if( url['@type'].includes('URL_mendeley') ) {
+            url.icon = 'fa-mendeley';
+          } else if( url['@type'].includes('URL_rssfeed') ) {
+            url.icon = 'fa-square-rss';
+          } else if( url['@type'].includes('URL_figshare') ) {
+            url.icon = 'ai-figshare';
           }
+        } catch(e) {
+          console.warn('error setting website icon', e);
         }
       });
 
