8b975964bfc8a46677645747b31f0332bf274299
Quinn Hart <qjhart@ucdavis.edu>
2024-04-30 15:56:09 -0700

    Update api(s) to be self describing openAPI with parameter checking (#437)
    * added one openapi route
    
    * update swagger package for search/browse/expert/sitefarm apis
    
    ---------
    
    Co-authored-by: UcDust <dcartwright@ucdavis.edu>

--- services/base-service/models/sitefarm/index.js
+++ services/base-service/models/sitefarm/index.js
@@ -4,5 +4,5 @@ module.exports = {
   model: new SiteFarmModel(),
   schema: require('../base/schema/minimal.json'),
   transform: require('../base/transform.js'),
-  swagger: 'sitefarm.yaml'
+  swagger: 'swagger.json'
 }
