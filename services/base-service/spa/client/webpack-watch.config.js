0d50159490f6a681fa75e87298518d78a8e8302a
Dusty Cartwright <103536473+UcDust@users.noreply.github.com>
2024-07-02 10:20:46 -0700

    split bundles by page to improve performance #514 (#515)

--- services/base-service/spa/client/webpack-watch.config.js
+++ services/base-service/spa/client/webpack-watch.config.js
@@ -4,17 +4,17 @@ let config = require('@ucd-lib/cork-app-build').watch({
     // path to your entry .js file
     entry : 'public/index.js',
     // folder where bundle.js will be written
-    preview : 'public',
+    preview : 'public/js',
     // path your client (most likely installed via yarn) node_modules folder.
-    // Due to the flat:true flag of yarn, it's normally best to separate 
+    // Due to the flat:true flag of yarn, it's normally best to separate
     // client code/libraries from all other modules (ex: build tools such as this).
     // will take an array of relative paths as well
     clientModules : 'public/node_modules'
   });
-  
+
   // optionaly you can run:
   // require('@ucd-lib/cork-app-build').watch(config, true)
   // Adding the second flag will generate a ie build as well as a modern
   // build when in development.  Note this slows down the build process.
-  
+
   module.exports = config;
