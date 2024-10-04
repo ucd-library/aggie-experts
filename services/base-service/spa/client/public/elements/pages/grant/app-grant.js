db0ab56b8bdbcc98afb5f79c6b4afc1ae21025e9
Dusty Cartwright <103536473+UcDust@users.noreply.github.com>
2024-10-04 10:37:23 -0700

    [FEAT] add contributors to grant detail page when no ae profile #558 (#621)

--- services/base-service/spa/client/public/elements/pages/grant/app-grant.js
+++ services/base-service/spa/client/public/elements/pages/grant/app-grant.js
@@ -84,12 +84,31 @@ export default class AppGrant extends Mixin(LitElement)
    * @return {Object} e
    */
   async _onGrantUpdate(e) {
+    if( e.state === 'error' ) {
+      this.dispatchEvent(
+        new CustomEvent("show-404", {})
+      );
+      return;
+    }
     if( e.state !== 'loaded' ) return;
     if( this.AppStateModel.location.page !== 'grant' ) return;
     if( e.grantId !== this.grantId ) return;
 
     let grantGraph = (e.payload['@graph'] || []).filter(g => g['@id'] === this.grantId)?.[0] || {};
-    let contributorsGraph = (e.payload['@graph'] || []).filter(g => g['@id'] !== this.grantId) || [];
+    if( !grantGraph ) return;
+
+    // Invisible grants still have resolvable landing pages. When the grant visibility has been changed, the expected behavior is:
+    // 1. If there are no public relationships, the grant landing page should return a 404 for all users, including the owner of the grant
+    let hasPublicRelationships = (grantGraph?.relatedBy || []).some(r => (r['inheres_in'] && r['is-visible']) || r['@id'].includes('#roleof_'));
+    if( !hasPublicRelationships ) {
+      this.dispatchEvent(
+        new CustomEvent("show-404", {})
+      );
+      return;
+    }
+
+    let aeContributors = (e.payload['@graph'] || []).filter(g => g['@id'] !== this.grantId) || [];
+    let otherContributors = [];
 
     this.grantName = grantGraph.name || '';
     this.awardedBy = grantGraph.assignedBy?.name || '';
@@ -110,39 +129,101 @@ export default class AppGrant extends Mixin(LitElement)
     this.coPis = [];
     this.leaders = [];
     this.researchers = [];
-    contributorsGraph.forEach(contributor => {
+
+    let contributors = (grantGraph.relatedBy || []);
+    if( !Array.isArray(contributors) ) contributors = [contributors];
+
+    otherContributors = contributors.filter(c => !c['inheres_in']);
+
+    aeContributors.forEach(contributor => {
       let name = contributor.contactInfo[0]?.name;
       let subtitle = name.split('§')?.pop()?.trim() || '';
       name = name.split('§')?.shift()?.trim() || '';
 
       let type = grantGraph.relatedBy.filter(r => r['inheres_in'] === contributor['@id'])?.[0]?.['@type'];
       if( !Array.isArray(type) ) type = [type];
+
+      // 2. If there are other experts related to the grant, the grant landing page will continue to resolve, but
+      //   --if the expert who changed the visibility setting is a PI or coPI,
+      //        their name will continue to appear in the list of contributors, but not link back to their profile page
+      //   --if that expert is a different type of contributor, they will be removed from the list of contributors
+      let isVisible = grantGraph.relatedBy.filter(r => r['inheres_in'] === contributor['@id'])?.[0]?.['is-visible'];
+
       if( type.includes('PrincipalInvestigatorRole') ) {
         this.pis.push({
+          hasProfile : isVisible,
           id : contributor['@id'],
           name,
           subtitle
         });
       } else if( type.includes('CoPrincipalInvestigatorRole') ) {
         this.coPis.push({
+          hasProfile : isVisible,
           id : contributor['@id'],
           name,
           subtitle
         });
-      } else if( type.includes('LeaderRole') ) {
+      } else if( isVisible && type.includes('LeaderRole') ) {
         this.leaders.push({
+          hasProfile : true,
           id : contributor['@id'],
           name,
           subtitle
         });
-      } else {
+      } else if( isVisible ) {
         this.researchers.push({
+          hasProfile : true,
           id : contributor['@id'],
           name,
           subtitle
         });
       }
     });
+
+    otherContributors.forEach(contributor => {
+      let type = contributor['@type'];
+      if( !Array.isArray(type) ) type = [type];
+
+
+      // pull in relates use id? but includes #roleof_
+      let relates = contributor['relates'];
+      if( !Array.isArray(relates) ) relates = [relates];
+
+      let personArk = contributor['@id']?.replace('roleof_', '') || '';
+      let name = relates.filter(r => r['@id'] === personArk)?.[0]?.name || '';
+
+      if( type.includes('PrincipalInvestigatorRole') ) {
+        this.pis.push({
+          hasProfile : false,
+          id : personArk,
+          name,
+          subtitle: ''
+        });
+      } else if( type.includes('CoPrincipalInvestigatorRole') ) {
+        this.coPis.push({
+          hasProfile : false,
+          id : personArk,
+          name,
+          subtitle: ''
+        });
+      } else if( type.includes('LeaderRole') ) {
+        this.leaders.push({
+          hasProfile : false,
+          id : personArk,
+          name,
+          subtitle: ''
+        });
+      } else {
+        this.researchers.push({
+          hasProfile : false,
+          id : personArk,
+          name,
+          subtitle: ''
+        });
+      }
+
+    });
+
   }
 
 }
