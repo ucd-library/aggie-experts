0187aceadc1dd7e08d82875b5ec22ae8f7c54aa1
rakunkel-ucd <rakunkel@ucdavis.edu>
2024-10-23 15:49:42 -0700

    Fix grant-feed and refine log get

--- harvest/experts-client/bin/experts-grant-feed-get-logs.js
+++ harvest/experts-client/bin/experts-grant-feed-get-logs.js
@@ -5,7 +5,9 @@
 
 import { Command } from '../lib/experts-commander.js';
 import { GoogleSecret } from '@ucd-lib/experts-api';
+import { Storage } from '@google-cloud/storage';
 import fs from 'fs';
+import path from 'path';
 import Client from 'ssh2-sftp-client';
 
 // Trick for getting __dirname in ES6 modules
@@ -26,8 +28,14 @@ program
   .option('-u, --username <username>', 'SFTP username', 'ucdavis')
   .option('-sp, --secretpath <secretpath>', 'Secret Manager secret path', 'projects/325574696734/secrets/Symplectic-Elements-FTP-ucdavis-password')
   .option('-do, --offset <offset>', 'Offset(days) to the most recent directory', 0)
+  .option('-ln, --logName <logName>', 'Log name to retreive', 'UCDavis_Grants_Feed_logs')
   .option_log()
   .parse(process.argv);
+
+// Creates a client
+const storage = new Storage();
+
+// Log names: UCDavis_Grants_Feed_logs UCDavis_Delete_Grants_Records_Logs UCDavis_Delete_User_Links_Logs
 
 let opt = await program.opts();
 
@@ -49,6 +57,28 @@ const ftpConfig = {
 
 const sftp = new Client();
 
+const slackWebhookUrl = '***REMOVED***';
+
+async function postToSlack(message) {
+  try {
+    const response = await fetch(slackWebhookUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text: message })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error posting message to Slack: ${response.statusText}`);
+    }
+
+    log.info(`Message posted to Slack: ${message}`);
+  } catch (error) {
+    log.error('Error posting message to Slack:', error.message);
+  }
+}
+
 async function downloadFilesFromMostRecentDirectory(remoteFolderPath, localFolderPath, offset = 0) {
   try {
     await sftp.connect(ftpConfig);
@@ -68,12 +98,29 @@ async function downloadFilesFromMostRecentDirectory(remoteFolderPath, localFolde
 
     log.info(`Most recently created directory: ${mostRecentDirectoryPath}`);
 
+    // Download files from the most recent directory that are txt files
+
     const files = await sftp.list(mostRecentDirectoryPath);
-    for (const file of files) {
+    const textFiles = files.filter(file => file.name.endsWith('.txt'));
+
+    for (const file of textFiles) {
       const remoteFilePath = `${mostRecentDirectoryPath}/${file.name}`;
       const localFilePath = `${localFolderPath}/${file.name}`;
       await sftp.get(remoteFilePath, fs.createWriteStream(localFilePath));
       log.info(`File downloaded successfully: ${remoteFilePath} -> ${localFilePath}`);
+      // If the filename contains 'error', 'summary', or 'notes' send it via email
+      if (file.name.toLowerCase().includes('error') || file.name.toLowerCase().includes('summary') || file.name.toLowerCase().includes('notes')) {
+        log.info('Copying to bucket -' + file.name);
+        // copy the file to the GCS bucket
+        uploadFileGCS(localFilePath, file.name);
+        if (file.name.toLowerCase().includes('error') || file.name.toLowerCase().includes('summary')) {
+          // Read the file and send it via email
+          const fileContent = fs.readFileSync(localFilePath, 'utf8');
+          // Post to the Slack webhook URL
+          log.info('Posting to Slack');
+          postToSlack(fileContent);
+        }
+      }
     }
   } catch (error) {
     log.error('Error downloading files from the most recent directory:', error.message);
@@ -82,14 +129,29 @@ async function downloadFilesFromMostRecentDirectory(remoteFolderPath, localFolde
   }
 }
 
-// Ensure the output directory exists
-if (!fs.existsSync(opt.output)) {
-  fs.mkdirSync(opt.output, { recursive: true });
+async function uploadFileGCS(localFilePath, destinationFileName) {
+  try {
+    const bucketName = 'aggie-enterprise'; // Replace with your GCS bucket name
+    const bucket = storage.bucket(bucketName);
+    const destination = path.join('grants/Logs', opt.env, opt.logName, destinationFileName); // Adjust the path in the bucket as needed
+
+    await bucket.upload(localFilePath, {
+      destination: destination,
+    });
+
+    log.info(`File uploaded to GCS: ${localFilePath} -> gs://${bucketName}/${destination}`);
+  } catch (error) {
+    log.error('Error uploading file to GCS:', error.message);
+  }
 }
 
-// Example usage
-const remoteFolderPath = '/PROD/Logs/UCDavis_Grants_Feed_logs';
-const localFolderPath = opt.output;
+// Ensure the output directory exists
+if (!fs.existsSync(opt.output + '/' + opt.prefix + opt.logName)) {
+  fs.mkdirSync(opt.output + '/' + opt.prefix + opt.logName, { recursive: true });
+}
+
+const remoteFolderPath = '/PROD/Logs/' + opt.logName; // UCDavis_Grants_Feed_logs;
+const localFolderPath = opt.output + '/' + opt.prefix + opt.logName;
 
 ftpConfig.password = await gs.getSecret(opt.secretpath);
 
