c7586b4d6e17b0720e1264157161b1caaa5415d4
Roger Alan Kunkel <125080780+rakunkel-ucd@users.noreply.github.com>
2024-12-23 12:05:17 -0800

    [FEAT] Add lastmodified to base model Index  (#648)

---
+++ services/base-service/models/sitefarm/template/modified-date.js
@@ -0,0 +1,42 @@
+template = {
+  id: "modified-date",
+  script: {
+    "lang": "mustache",
+    "source": `{
+      "query": {
+        "bool": {
+          "filter": {
+            "bool": {
+              "must": [
+                {
+                  "bool": {
+                    "must": [
+                      { "exists": { "field": "is-visible" }},
+                      { "term" : {"is-visible": true } }
+                    ]
+                  }
+                }
+                {{#gte_date}}
+                ,{
+                  "range": {
+                    "modified-date": {
+                      "gte": "{{gte_date}}"
+                    }
+                  }
+                }
+                {{/gte_date}}
+                ,{
+                  "terms": {
+                      "@id": {{#toJson}}expert{{/toJson}}
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }`
+  }
+
+}
+module.exports = template;
