403ae1b73107e4c167452e28f9ff5e3d20347b4e
Quinn Hart <qjhart@ucdavis.edu>
2025-02-06 13:09:54 -0800

    All authorships.  Fixes #717

--- services/base-service/models/work/model.js
+++ services/base-service/models/work/model.js
@@ -122,9 +122,11 @@ class WorkModel extends BaseModel {
 
     // combine relatedBy (ordered)
     function addRelatedBy(node) {
+      console.log('Adding relatedBy', node['@id']);
       if (node.relatedBy) {
         Array.isArray(node.relatedBy) || (node.relatedBy = [node.relatedBy]);
         node.relatedBy.forEach((rel) => {
+          console.log('Adding relatedBy', rel['@id']);
           relatedBy[rel['@id']] = rel;
           });
       }
@@ -133,6 +135,7 @@ class WorkModel extends BaseModel {
     try {
       let existing = await this.client_get(root_node['@id']);
       existing = this.get_expected_model_node(existing);
+      console.log('WorkModel.update: existing', existing);
       addRelatedBy(existing);
     } catch(e) {
       //logger.info(`WorkModel.update: ${root_node['@id']} not found`);
@@ -157,6 +160,7 @@ class WorkModel extends BaseModel {
           }
         }
     }
+    root_node.relatedBy = Object.values(relatedBy);
     const doc = this.promote_node_to_doc(root_node);
     await this.update_or_create_main_node_doc(doc);
     const work_snippet = this.snippet(root_node);
