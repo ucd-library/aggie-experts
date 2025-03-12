ac0fb215c0119c9fe45dcb2ca3fd373203f4aa6f
Dusty Cartwright <dcartwright@ucdavis.edu>
2025-03-12 15:47:35 -0700

    update expert api get route to use item_endpoint mw

--- services/base-service/models/expert/api.js
+++ services/base-service/models/expert/api.js
@@ -8,7 +8,7 @@ const md5 = require('md5');
 // const { logger } = require('@ucd-lib/fin-service-utils');
 const model= new ExpertModel();
 
-const { browse_endpoint } = require('../middleware/index.js');
+const { browse_endpoint, item_endpoint } = require('../middleware/index.js');
 const { openapi, json_only, user_can_edit, is_user } = require('../middleware/index.js')
 
 function subselect(req, res, next) {
@@ -228,33 +228,10 @@ function expert_valid_path_error(err, req, res, next) {
   })
 }
 
+item_endpoint(router,model,subselect)
+
 router.route(
   '/:expertId'
-).get(
-  is_user,
-  expert_valid_path(
-    {
-      description: "Get an expert by id",
-      responses: {
-        "200": openapi.response('Expert'),
-        "404": openapi.response('Expert_not_found')
-      }
-    }
-  ),
-  expert_valid_path_error,
-  async (req, res, next) => {
-    let expertId = `expert/${req.params.expertId}`;
-    try {
-      res.thisDoc = await model.get(expertId);
-      next();
-    } catch (e) {
-      return res.status(404).json(`${req.path} resource not found`);
-    }
-  },
-  subselect, // filter results
-  (req, res) => {
-    res.status(200).json(res.thisDoc);
-  }
 ).post(
   is_user,
   expert_valid_path(
