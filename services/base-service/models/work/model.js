42797acf70250259c4e1bac0de6908543170df11
Dusty Cartwright <103536473+UcDust@users.noreply.github.com>
2025-04-22 17:18:57 -0700

    fix for item pageload failing in seo code (#815)

--- services/base-service/models/work/model.js
+++ services/base-service/models/work/model.js
@@ -82,7 +82,7 @@ class WorkModel extends BaseModel {
   }
 
   async seo(id) {
-    node = await this.get(id);
+    let node = await this.get(id);
     node = this.subselect(node);
     let seo = {"@context": node['@context'],
                 "@graph": []
