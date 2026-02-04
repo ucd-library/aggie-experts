60bf1c3146f5d28711c7b78cb132f09e08334537
Dusty Cartwright <dcartwright@ucdavis.edu>
2026-02-04 14:18:35 -0800

    remove extra data from the grant names/titles in the miv api #1107

--- services/base-service/models/miv/api.js
+++ services/base-service/models/miv/api.js
@@ -119,6 +119,10 @@ router.get(
               }
             });
           }
+
+          // trim to just the name
+          hit.name = (hit.name || '').split('§')?.[0]?.trim() || hit.name;
+
           grants.push({
             '@id': hit['@id'],
             title: hit.name,
@@ -168,6 +172,8 @@ router.get(
       const find = await expert.search(opts);
       let grants = [];
       for (const hit of find.hits[0]._inner_hits) {
+        // trim to just the name
+        hit.name = (hit.name || '').split('§')?.[0]?.trim() || hit.name;
         grants.push(hit);
       }
       res.send(grants);
