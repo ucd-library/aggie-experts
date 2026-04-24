4b94530790e331c6f914dc7001d1f2b2a4cfb6f3
Dusty Cartwright <dcartwright@ucdavis.edu>
2026-04-24 12:14:36 -0700

    start potential swagger api refactor #1161

--- webapp/lib/api.js
+++ webapp/lib/api.js
@@ -1,107 +1,95 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// NOTE: swagger-jsdoc based generation is currently disabled.
-// Express 5 + the mixed swagger/openapi approaches in this repo make the generated
-// doc misleading (empty paths/components). We'll serve a simple index instead.
-// const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerJSDoc = require('swagger-jsdoc');
 const { logger, config } = require('@ucd-lib/experts-commons');
 const { initAuth } = require('../models/middleware/index.js');
 const models = require('./models.js');
 const keycloak = require('./keycloak.js');
+const swaggerParameters = require('./swagger/parameters.json');
+const swaggerSchemas = require('./swagger/schemas.json');
+const swaggerResponses = require('./swagger/responses.json');
+const swaggerRequestBodies = require('./swagger/requestBodies.json');
 
 const app = express();
 
 app.use(keycloak.setUser);
 
-// const swaggerDefinition = {
-//   openapi: '3.0.0',
-//   info: {
-//     title: 'FIN API',
-//     version: '1.0.0',
-//     description:
-//       'This is the Swagger documentation for FIN.',
-//     license: {
-//       name: 'Licensed Under MIT',
-//       url: 'https://spdx.org/licenses/MIT.html',
-//     },
-//     contact: {
-//       name: 'Online Strategy - UC Davis Library',
-//       url: 'https://library.ucdavis.edu/online-strategy/',
-//     },
-//   },
-//   paths: {}
-// };
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  "info": {
+    "title": "Aggie Experts API",
+    "version": "5.0", // TODO pull from config file for version?
+    "description": "Allows for the retrieval of expert information.",
+    "termsOfService": "https://experts.ucdavis.edu/termsofuse",
+    "contact": {
+      "email": "experts@ucdavis.edu"
+    },
+    "license": {
+      "name": "Apache 2.0",
+      "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+    }
+  },
+  "servers": [
+    {
+      "url": "http://experts.ucdavis.edu/api/sitefarm"
+    }
+  ],
+  "tags": [
+    {
+      "name": "expert",
+      "description": "Expert Information"
+    },
+  ],
+  components: {
+    parameters: swaggerParameters,
+    schemas: swaggerSchemas,
+    responses: swaggerResponses,
+    requestBodies: swaggerRequestBodies
+  },
+  paths: {}
+};
 
 async function init() {
-  // let apis = [];
+  let apis = [];
   let names = await models.names();
   logger.info(`Found ${names.length} API(s)`, { names });
   for( let name of names ) {
-    let {api/*, swagger*/} = await models.get(name);
+    let {api, swagger} = await models.get(name);
     logger.info(`Found API for ${name}`, { api: !!api });
     if( !api ) continue;
 
-    // try {
-    //   if( swagger?.paths ) {
-    //     if( !Array.isArray(swagger.paths) ) {
-    //       swagger.paths = Object.entries(swagger.paths).map(([key, value]) => ({ path : key, docs : value }));
-    //     }
-    //     swagger.paths.forEach(doc => {
-    //       swaggerDefinition.paths[`/api/${doc.path.replace(/\/?api\/?/g, '')}`] = doc.docs;
-    //     });
-    //   }
-    //   if( swagger?.components?.schemas ) {
-    //     swaggerDefinition.components = swagger.components;
-    //   }
-    //   if( swagger?.openapi ) {
-    //     swaggerDefinition.openapi = swagger.openapi;
-    //   }
-    //   if( swagger?.info ) {
-    //     swaggerDefinition.info = swagger.info;
-    //   }
-    //   apis.push('api/'+name);
-    // } catch (e) {
-    //   logger.error('Error loading swagger for '+name, e);
-    // }
+    try {
+      if( swagger?.paths ) {
+        if( !Array.isArray(swagger.paths) ) {
+          console.log('adding paths for', name, Object.keys(swagger.paths));
+          swagger.paths = Object.entries(swagger.paths).map(([key, value]) => ({ path : key, docs : value }));
+        }
+        swagger.paths.forEach(doc => {
+          console.log('adding paths for', name, Object.keys(doc.docs));
+          swaggerDefinition.paths[`/api/${doc.path.replace(/\/?api\/?/g, '')}`] = doc.docs;
+        });
+      }
+      apis.push('api/'+name);
+    } catch (e) {
+      logger.error('Error loading swagger for '+name, e);
+    }
 
     logger.info(`Registering api routes for ${name} at /api/${name}`);
     app.use('/'+name, bodyParser.json(), api);
   }
 
-  // const options = {
-  //   swaggerDefinition,
-  //   apis,
-  // };
+  const options = {
+    swaggerDefinition,
+    apis,
+  };
 
-  // const swaggerSpec = swaggerJSDoc(options);
+  const swaggerSpec = swaggerJSDoc(options);
 
-  // app.get('/', (req, res) => {
-  //   res.json(swaggerSpec);
-  // });
+  app.get('/', (req, res) => {
+    res.json(swaggerSpec);
+  });
 
   await initAuth();
-  
-  // API index
-  app.get('/', async (req, res) => {
-    // Re-fetch names to ensure we reflect what's enabled
-    const names = await models.names();
-
-    const docs = {
-      message: 'Aggie Experts API index',
-      openapi: {
-        expert: '/api/expert/openapi.json',
-        sitefarm: '/api/sitefarm/openapi.json',
-        miv: '/api/miv/openapi.json'
-      },
-      apis: names.map(n => `/api/${n}`),
-      contact: {
-      name: 'Online Strategy - UC Davis Library',
-      url: 'https://library.ucdavis.edu/online-strategy/',
-      }
-    };
-
-    res.json(docs);
-  });
 
   return app;
 }
