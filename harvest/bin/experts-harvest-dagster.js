6d98b033a191ec662e54a4f8c3e8048b82aeb254
Michael Grow <michaelgrow@gmail.com>
2026-06-10 10:04:38 -0700

    adding pg module import back in after merge

--- harvest/bin/experts-harvest-dagster.js
+++ harvest/bin/experts-harvest-dagster.js
@@ -1,11 +1,12 @@
 import { Command } from 'commander';
 import CdlClient from '../lib/extract/cdl.js';
 import DagsterAPI from '../lib/dagster/api.js';
-import { 
+import {
   getYearWeek,
   logger,
   config
 } from '@ucd-lib/experts-commons';
+import PgClient from '../lib/pg-client.js';
 import cache from '../lib/cache.js';
 import path from 'path';
 const program = new Command();
