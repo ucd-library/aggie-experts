ad6cfb0ed9e375a85c3c89e47349a0227fd559ee
Dusty Cartwright <dcartwright@ucdavis.edu>
2026-07-06 14:05:23 -0700

    update for tablet resolution ui styles for browse pages

--- webapp/spa/client/public/elements/pages/browse/app-browse-by.tpl.js
+++ webapp/spa/client/public/elements/pages/browse/app-browse-by.tpl.js
@@ -535,6 +535,7 @@ return html`
 
     @media (max-width: 992px) {
       .browse-container {
+        width: 90%;
         padding-right: 0;
         padding-left: 0;
         gap: 0rem 2rem;
@@ -610,6 +611,7 @@ return html`
       .refine-search-mobile {
         display: block;
         width: 100%;
+        padding-left: .25rem;
       }
     }
 
