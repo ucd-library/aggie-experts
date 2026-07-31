e0addb060b2a64cf3017978482a51b94c21643cd
Mike Grow <74577799+mgrow1@users.noreply.github.com>
2026-07-31 12:26:57 -0700

    remove commons from bundle (#1348)
    * remove commons from bundle and fetch ORG_LOOKUP
    
    * import `dept-utils` via relative path; access org lookup client side using `APP_CONFIG`
    
    * clean up
    
    * console warn if APP_CONFIG is missing org lookup

--- commons/index.js
+++ commons/index.js
@@ -1,6 +1,9 @@
 import { Elasticsearch, getEsClient } from './lib/elasticsearch/index.js';
 import { ORG_LOOKUP } from './lib/org-lookup.js';
-import { expandDeptParam, deptCodesToOfficialNames } from './lib/dept-utils.js';
+import {
+  expandDeptParam as _expandDeptParam,
+  deptCodesToOfficialNames as _deptCodesToOfficialNames
+} from './lib/dept-utils.js';
 import { logger, logReqMiddleware, createLogger } from './lib/logger.js';
 import GoogleSecret from './lib/google-secret.js';
 import SlackNotifier from './lib/slack-notifier.js';
@@ -8,6 +11,14 @@ import config from './lib/config.js';
 import { getYearWeek, getTodaysDate, isPlainDate, parseYearWeek } from './lib/year-week.js';
 import ElementsClient from './lib/elements-client.js';
 import ExpertsKcAdminClient from './lib/keycloak-admin.js';
+
+// dept-utils is data-injected (see commons/lib/dept-utils.js). Server-side callers
+// don't carry the ORG_LOOKUP table around, so bind it here to preserve the original
+// (dept, deptCodesIncluded, deptCodesExcluded) / (codes) signatures.
+const expandDeptParam = (dept, deptCodesIncluded, deptCodesExcluded) =>
+  _expandDeptParam(ORG_LOOKUP, dept, deptCodesIncluded, deptCodesExcluded);
+const deptCodesToOfficialNames = (codes) =>
+  _deptCodesToOfficialNames(ORG_LOOKUP, codes);
 
 
 export {
