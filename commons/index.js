79ff72d671f3e176cc7bf3daa853192406b04e01
Michael Grow <michaelgrow@gmail.com>
2026-07-31 10:47:27 -0700

    clean up

--- commons/index.js
+++ commons/index.js
@@ -4,6 +4,13 @@ import {
   expandDeptParam as _expandDeptParam,
   deptCodesToOfficialNames as _deptCodesToOfficialNames
 } from './lib/dept-utils.js';
+import { logger, logReqMiddleware, createLogger } from './lib/logger.js';
+import GoogleSecret from './lib/google-secret.js';
+import SlackNotifier from './lib/slack-notifier.js';
+import config from './lib/config.js';
+import { getYearWeek, getTodaysDate, isPlainDate, parseYearWeek } from './lib/year-week.js';
+import ElementsClient from './lib/elements-client.js';
+import ExpertsKcAdminClient from './lib/keycloak-admin.js';
 
 // dept-utils is data-injected (see commons/lib/dept-utils.js). Server-side callers
 // don't carry the ORG_LOOKUP table around, so bind it here to preserve the original
@@ -12,13 +19,6 @@ const expandDeptParam = (dept, deptCodesIncluded, deptCodesExcluded) =>
   _expandDeptParam(ORG_LOOKUP, dept, deptCodesIncluded, deptCodesExcluded);
 const deptCodesToOfficialNames = (codes) =>
   _deptCodesToOfficialNames(ORG_LOOKUP, codes);
-import { logger, logReqMiddleware, createLogger } from './lib/logger.js';
-import GoogleSecret from './lib/google-secret.js';
-import SlackNotifier from './lib/slack-notifier.js';
-import config from './lib/config.js';
-import { getYearWeek, getTodaysDate, isPlainDate, parseYearWeek } from './lib/year-week.js';
-import ElementsClient from './lib/elements-client.js';
-import ExpertsKcAdminClient from './lib/keycloak-admin.js';
 
 
 export {
